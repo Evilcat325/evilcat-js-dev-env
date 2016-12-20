23c9d6f8cbe4eae1fd765b57da8f9b22f5300159
Sen Li <ls.learnshare@gmail.com>
2016-12-20 17:21:41 -0500

    setup eslint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,26 +2,28 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
-import config from '../webpack.config.dev';
 import webpackdev from 'webpack-dev-middleware';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
-app.use(webpackdev(compiler,{
+app.use(webpackdev(compiler, {
   noInfo: true,
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
 }));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
-})
+});
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
   } else {
-    open('http://localhost:' + port);
+    open(`http://localhost:${port}`);
   }
-})
+});
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,19 +5,19 @@ export default {
   devtool: 'inline-source-map',
   noInfo: false,
   entry: [
-    path.resolve(__dirname, 'src/index')
+    path.resolve(__dirname, 'src/index'),
   ],
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'src'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   plugins: [],
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
-    ]
-  }
-}
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.css$/, loaders: ['style', 'css'] },
+    ],
+  },
+};
