3d7c45431720e6b9bef259de18c613c8967ae27d
Sen Li <ls.learnshare@gmail.com>
2016-12-20 15:51:39 -0500

    Setup webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+import webpackdev from 'webpack-dev-middleware';
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(webpackdev(compiler,{
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
