d4af945972e45b5f8458bc11a96454c33bb32f0e
Sen Li <ls.learnshare@gmail.com>
2017-01-04 17:55:00 -0500

    add prod webpack config

---
+++ webpack.config.prod.js
@@ -0,0 +1,34 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index'),
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    // Eliminate duplicat packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+
+    // Minify js
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ['babel'],
+    }, {
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+    }],
+  },
+};
